3e5837b8be6ad1fe35eea9fc34decee4ba6d69c7
Edeotena <timvaliul@mail.ru>
2021-03-08 12:38:43 +0300

    Homework-1

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,5 +1,5 @@
-#ifndef PREP_2021_01_PROJECT_INCLUDE_UTILS_H_
-#define PREP_2021_01_PROJECT_INCLUDE_UTILS_H_
+#ifndef PROJECT_INCLUDE_UTILS_H_
+#define PROJECT_INCLUDE_UTILS_H_
 
 size_t custom_strlen(const char* str);
 int custom_pow(int base, int power);
