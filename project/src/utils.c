16446479491e682d9b988ffb08ca7f3c672740f8
Edeotena <timvaliul@mail.ru>
2021-03-09 16:33:49 +0300

    HomeWork 1 Reworked

--- project/src/utils.c
+++ project/src/utils.c
@@ -7,9 +7,12 @@ size_t custom_strlen(const char* str) {
 }
 
 int custom_pow(int base, int power) {
-	if (power == 0) return 1;
-	if (base == 1) return 1;
-	if (base == 0) return 0;
+	if (power == 0 || base == 1) {
+		return 1;
+	}
+	if (base == 0) {
+		return 0;
+	}
 	int result = base;
 	for(int i = 1; i < power; i++) result *= base;
 	return result;
