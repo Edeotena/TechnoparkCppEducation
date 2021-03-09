89fe7348dd6981c7145382c10eb4e511a848f416
Edeotena <timvaliul@mail.ru>
2021-03-09 17:59:46 +0300

    HomeWork 1 Reworked

--- project/src/prime.c
+++ project/src/prime.c
@@ -1,7 +1,9 @@
 #include "prime.h"
 
 int is_Prime(long value) {
-	if (value <= 1) return 0;
+	if (value <= 1) {
+		return 0;
+	}
 	for (int i = 2; i * i <= value; i++) {
 		 if (value % i == 0) {
 			return 0;
--- project/src/utils.c
+++ project/src/utils.c
@@ -14,6 +14,8 @@ int custom_pow(int base, int power) {
 		return 0;
 	}
 	int result = base;
-	for(int i = 1; i < power; i++) result *= base;
+	for(int i = 1; i < power; i++) {
+		result *= base;
+	}
 	return result;
 }
