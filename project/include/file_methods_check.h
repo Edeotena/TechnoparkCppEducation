70e1df849ed15ef334ea96a4491f73321d83aa2d
Edeotena <timvaliul@mail.ru>
2021-03-17 20:59:32 +0300

    created file_methods_check.h

--- project/include/file_methods_check.h
+++ project/include/file_methods_check.h
@@ -1,5 +1,5 @@
-#ifndef PROJECT_INCLUDE_FILE_OPEN_READ_CHECK_H_
-#define PROJECT_INCLUDE_FILE_OPEN_READ_CHECK_H_
+#ifndef PROJECT_INCLUDE_FILE_METHODS_CHECK_H_
+#define PROJECT_INCLUDE_FILE_METHODS_CHECK_H_
 
 #include <string.h>
 #include <stdio.h>
@@ -14,4 +14,4 @@ void write_to_file(const char* filename_, test_struct* data);
 void read_from_file(const char* filename_, test_struct* data);
 int file_methods_check(const char* filename_);
 
-#endif  //  PROJECT_INCLUDE_FILE_OPEN_READ_CHECK_H_
+#endif  //  PROJECT_INCLUDE_FILE_METHODS_CHECK_H_
