222d709741934a8f7f259def429d6f4b5da80c0d
Edeotena <timvaliul@mail.ru>
2021-04-10 21:51:17 +0300

    HW-5

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/timur/prep-2021.01
+# Build directory: /home/timur/prep-2021.01
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("project")
