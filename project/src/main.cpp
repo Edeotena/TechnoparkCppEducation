f4205b2ca64aeb4bb6d011273787fbafdf7577be
Тимур Валиуллин <nafigzhiti@mail.ru>
2021-04-22 19:35:37 +0000

    Update main.cpp

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
+#include "map.h"
 
 
-int main(int /*argc*/, const char** /*argv*/) {
-	std::cout << "Hi, the great game of HW-5" << std::endl;
+int main(int argc, const char** argv) {
+    if (argc != 3 || strcmp(argv[1], "--map") != 0) {
+        return WRONG_INPUT;
+    }
+
+    map::map new_map(argv[2]);
+    std::string action;
+
+    do {
+        if (new_map.show_actions() && getline(std::cin, action)) {
+            std::cout << std::endl;
+        } else {
+            break;
+        }
+        new_map.make_actions(action);
+        if (new_map.get_player_health() <= 0) {
+            std::cout << "player died\n";
+            break;
+        }
+    } while (true);
+
     return 0;
 }
