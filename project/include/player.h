119f00abb8aeb72b10f842b93e1a5391c8067f65
Тимур Валиуллин <nafigzhiti@mail.ru>
2021-04-23 21:18:20 +0000

    STAGE2 COMMIT (probably)

--- project/include/player.h
+++ project/include/player.h
@@ -5,8 +5,14 @@ class player {
  private:
     int health = 100;
     int x = 0, y = 0;
+    int armor_count = 0, weight = 0;
+    bool armor_equipment[5];
 
  public:
+    player();
+
+    ~player() = default;
+
     void move_x(const int x);
 
     void move_y(const int y);
@@ -18,6 +24,13 @@ class player {
     int get_health() const;
 
     void take_damage(const int damage);
-};
 
+    int get_armor() const;
+
+    bool have_armor(const int armor_number) const;
+
+    bool add_armor(const int armor_number, const int armor_count, const int armor_weight);
+
+    bool throw_armor(const int armor_number, const int armor_count, const int armor_weight);
+};
 }  // namespace player
